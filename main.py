74de5a3f154bb9efae08b3881b10f5a6ec021816
crovetonite <118976898+crovetonite@users.noreply.github.com>
2022-12-19 18:02:17 +0500

    Update main.py
    game cycle and time

--- main.py
+++ main.py
@@ -11,3 +11,17 @@ screen = pygame.display.set_mode((width, height))
 txt_font = pygame.freetype.SysFont('Comic Sans MS', 25)
 font = pygame.font.SysFont('Arial', 20)
 objects=[]
+
+
+#time and score
+start_ticks = pygame.time.get_ticks()
+score = 0
+check = False
+timing = 10
+#game cycle
+while True:
+    screen.fill((30, 30, 30))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
