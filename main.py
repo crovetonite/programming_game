a03b38b43ab050fe861d78613da0e9320c7cf81d
crovetonite <118976898+crovetonite@users.noreply.github.com>
2022-12-19 17:33:27 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import random
 pygame.init()
 
 #settings
+pygame.init()
+fps = 60
+fpsClock = pygame.time.Clock()
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+txt_font = pygame.freetype.SysFont('Comic Sans MS', 25)
+font = pygame.font.SysFont('Arial', 20)
+objects=[]
